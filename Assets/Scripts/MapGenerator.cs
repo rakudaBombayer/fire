395bcbe2ff0a77ca86681a0d98fea6b789c1f6ee
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2023-09-16 17:56:17 +0900

    Map自動生成2

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -10,14 +10,25 @@ using UnityEngine;
 public class MapGenerator : MonoBehaviour
 {
     [SerializeField] TileObj tileObjPrefab;
+    [SerializeField] Transform tileParent;
 
+    const int WIDTH = 15;
+    const int HEIGHT = 9;
     private void Start()
-    {
+    {   
         Generate();
     }
 
     void Generate()
-    {
-        Instantiate(tileObjPrefab);
+    {   
+        Vector2 offset = new Vector2(-WIDTH/2, -HEIGHT/2);
+        for (int x = 0; x < WIDTH; x++)
+        {
+            for (int y = 0; y < HEIGHT; y++)
+            {   
+                Vector2 pos = new Vector2(x, y) + offset;
+                Instantiate(tileObjPrefab, pos, Quaternion.identity, tileParent);
+            }
+        }
     }
 }
